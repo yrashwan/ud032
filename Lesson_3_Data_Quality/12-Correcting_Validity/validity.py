08edcdacfa70ec9c513a28871d2d84cae85aa4da
Yasser Rashwan <yasser.csd@gmail.com>
2016-02-27 02:09:28 +0200

    add lesson 3 quiz solution

--- Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
+++ Lesson_3_Data_Quality/12-Correcting_Validity/validity.py
@@ -7,39 +7,68 @@ The following things should be done:
 - the rest of the fields and values should stay the same
 - if the value of the field is a valid year in range, as described above,
   write that line to the output_good file
-- if the value of the field is not a valid year, 
+- if the value of the field is not a valid year,
   write that line to the output_bad file
 - discard rows (neither write to good nor bad) if the URI is not from dbpedia.org
 - you should use the provided way of reading and writing data (DictReader and DictWriter)
   They will take care of dealing with the header.
 
-You can write helper functions for checking the data and writing the files, but we will call only the 
+You can write helper functions for checking the data and writing the files, but we will call only the
 'process_file' with 3 arguments (inputfile, output_good, output_bad).
 """
 import csv
-import pprint
+import re
 
 INPUT_FILE = 'autos.csv'
 OUTPUT_GOOD = 'autos-valid.csv'
 OUTPUT_BAD = 'FIXME-autos.csv'
+
+
+def writeCSVFile(fileName, header, rowList):
+    with open(fileName, "w") as f:
+        writer = csv.DictWriter(f, delimiter=",", fieldnames=header, lineterminator='\n')
+        writer.writeheader()
+        for row in rowList:
+            writer.writerow(row)
+
+
+def extractYear(year):
+    m = re.match('[0-9]+', year)
+    if m is None:
+        return -1
+    return m.group(0)
+
+
+def isValidYear(year):
+    return year >= 1886 and year <= 2014
+
+
+def isValidURI(uri):
+    return re.match('(http:\/\/)?(www.)?(dbpedia.org)', uri) is not None
+
 
 def process_file(input_file, output_good, output_bad):
 
     with open(input_file, "r") as f:
         reader = csv.DictReader(f)
         header = reader.fieldnames
+        goodRows = []
+        badRows = []
 
         #COMPLETE THIS FUNCTION
+        for row in reader:
+            yearField = row["productionStartYear"]
+            if not isValidURI(row["URI"]):
+                continue
+            year = int(extractYear(yearField))
+            if isValidYear(year):
+                row["productionStartYear"] = str(year)
+                goodRows.append(row)
+            else:
+                badRows.append(row)
 
-
-
-    # This is just an example on how you can use csv.DictWriter
-    # Remember that you have to output 2 files
-    with open(output_good, "w") as g:
-        writer = csv.DictWriter(g, delimiter=",", fieldnames= header)
-        writer.writeheader()
-        for row in YOURDATA:
-            writer.writerow(row)
+    writeCSVFile(output_good, header, goodRows)
+    writeCSVFile(output_bad, header, badRows)
 
 
 def test():
